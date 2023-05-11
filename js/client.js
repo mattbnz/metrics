89de60daad0d0b1a98f6f0e43434d686affaa15d
Matt Brown <matt@mattb.net.nz>
2023-05-12 11:47:28 +1200

    Add basic js client library

---
+++ js/client.js
@@ -0,0 +1,79 @@
+var view = Math.random().toString(36).substring(2, 12);
+var hadActivity = false;
+var scrollPerc = 0;
+
+var reportURL = "";
+
+function sendClick(target) {
+    SendMetric({"Event": "click", "Target": target, "SessionId": view})
+}
+
+function notifyClick(e) {
+    sendClick(e.target.id);
+}
+function notifyChange(e) {
+    SendMetric({"Event": "click", "Target": e.target.id, "Value": e.target.value, "SessionId": view})
+}
+
+function onMouse(e) {
+    hadActivity = true;
+}
+
+function onScroll(e) {
+    hadActivity = true;
+    var perc = Math.round((window.scrollY / document.body.scrollHeight) * 100);
+    if (perc != scrollPerc) {
+        scrollPerc = perc;
+    }
+}
+
+function reportActivity() {
+    if (hadActivity) {
+        hadActivity = false;
+        SendMetric({
+            "Event": "activity",
+            "SessionId": view,
+            "ScrollPerc": scrollPerc.toString(),
+         });
+    }
+}
+
+function addElementHandlers(className, eventName, handler) {
+    var elements = document.getElementsByClassName(className);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener(eventName, handler);
+    }
+}
+
+export async function SendMetric(data) {
+    if (reportURL == "") {
+        return;
+    }
+    // Fire and forget, don't care about response or success.
+    try {
+        fetch(reportURL, {
+            body : JSON.stringify(data),
+            method : 'POST',
+            referrerPolicy: "no-referrer-when-downgrade",
+            keepalive: true,
+        });
+    } catch (e) {
+        console.log("Failed to send metric to " + reportURL + ": " + e);
+    }
+}
+
+export function SetupMetrics(url, reportIntervalSecs) {
+    reportURL = url;
+    // Log the load
+    SendMetric({"Event": "pageview", "SessionId": view})
+    // Watch for events
+    addElementHandlers("notify-click", "click", notifyClick);
+    addElementHandlers("notify-change", "change", notifyChange);
+    // Watch for activity
+    document.addEventListener("scroll", onScroll);
+    document.addEventListener("mousemove", onMouse);
+    if (reportIntervalSecs < 20) {
+        reportIntervalSecs = 20;
+    }
+    setInterval(reportActivity, reportIntervalSecs*1000);
+}
